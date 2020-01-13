7137be95dad20d0576cdad7f740e7cbebb0a2bde
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-01-13 17:41:58 +0600

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -15,6 +15,20 @@ if (isset($_POST['login-submit'])) {
   else {
     // code...
     $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?;";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!sqli_stmt_prepare($stmt. $sql)) {
+      // code...
+      header("Location: ../index.php?error=sqlerror");
+      exit();
+    }
+    else {
+      // code...
+      mysqli_stmt_bind_param($stmt, "ss", $emailID, $password);
+      mysqli_stmt_execute($stmt);
+
+      $result = mysqli_stmt_get_result($stmt);
+    }
   }
 }
 else {
