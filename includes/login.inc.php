cfbb15d7929c939f227da8c197a1ca71c4025ac5
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-01-15 00:47:51 +0600

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -27,7 +27,7 @@ if (isset($_POST['login-submit'])) {
       mysqli_stmt_bind_param($stmt, "ss", $usernameID, $password);
       mysqli_stmt_execute($stmt);
 
-      $result = mysqli_stmt_get_result($stmt);
+      // $result = mysqli_stmt_get_result($stmt);
 
       if ($row = mysqli_fetch_assoc()) {
         // code...
